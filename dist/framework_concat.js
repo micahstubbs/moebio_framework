22c38a3d6248ec9b26cca26ee8a59f2db75cf30b
Santiago Ortiz <s@moebio.com>
2014-07-10 21:43:53 -0300

    in case

--- dist/framework_concat.js
+++ dist/framework_concat.js
@@ -17470,6 +17470,8 @@ TreeDraw._drawRectanglesTreeChildren = function(node, frame, colors, margin){
 TreeDraw.drawTreemap = function(frame, tree, colorList, weights, textColor){
 	var change = frame.memory==null || frame.memory.tree!=tree || frame.memory.width!=frame.width || frame.memory.height!=frame.height || frame.memory.weights!=weights;
 
+	c.l('[TM] -----------> nF', nF);
+
 	if(change){
 		var changeInTree = frame.memory!=null && frame.memory.tree!=null!=tree;
 		var changeInWeights = frame.memory!=null && frame.memory.weights!=weights;
@@ -17657,7 +17659,6 @@ TreeDraw.drawTreemap = function(frame, tree, colorList, weights, textColor){
 			}
 		}
 	});
-
 	
 	if(overNode){
 		rect = new Rectangle(tx(overNode._outRectangle.x), ty(overNode._outRectangle.y), overNode._outRectangle.width*kx, overNode._outRectangle.height*ky);
