4987a9a86d917c057642fcffdc3ba795d39b3414
Jim Vallandingham <vlandham@gmail.com>
2015-05-26 20:32:16 -0700

    add basic search to docs

---
+++ docs/moebio-jsdoc/static/scripts/filter.js
@@ -0,0 +1,50 @@
+var filter = function() {
+  var all;
+  var filter;
+
+  var filterInput;
+
+  var self = {};
+
+  self.setup = function() {
+    all = $(".side ul ul li");
+    filter = all;
+
+    filterInput = $("#filter-input");
+    filterInput.on("input", update);
+
+    var clearButton = $("#clear-filter-button");
+    clearButton.on("click", clear);
+  }
+
+  function update() {
+    var exp = new RegExp( filterInput.val(), 'gi' );
+    filter = all.filter(function(i) {
+      var name = $(this).find('a').text();
+      var res = name.match(exp);
+      if( res && res.length > 0 ) {
+        $(this).removeClass("filtered");
+      } else {
+        $(this).addClass("filtered");
+      }
+      console.log(name);
+      return true;
+
+    });
+  }
+
+  function clear(e) {
+    filterInput.val("");
+    update();
+    e.preventDefault();
+  }
+
+  return self;
+
+};
+
+$( document ).ready(function() {
+  var f = filter();
+  f.setup();
+
+});
