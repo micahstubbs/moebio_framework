d000ac02579248ef9caf7f5a89d3a0612ff24f2b
Jim Vallandingham <vlandham@gmail.com>
2015-06-15 10:04:14 -0700

    more tests

--- tests/Interval.test.js
+++ tests/Interval.test.js
@@ -19,10 +19,30 @@ describe("Interval", function() {
     // I think it should return 50.
     expect(inter.getInterpolatedValue(0.5)).toBe(-50);
   });
+
   it("should inverse interpolate", function() {
     var inter = new Interval(0, 100);
     expect(inter.getInverseInterpolatedValue(0)).toBe(0);
     // TODO: I don't understand why this should return -0.5.
     expect(inter.getInverseInterpolatedValue(50)).toBe(-0.5);
+  });
+
+  it("should find containing values", function() {
+    var inter = new Interval(0, 100);
+
+    expect(inter.contains(0)).toBe(true);
+    expect(inter.contains(100)).toBe(true);
+    expect(inter.contains(50)).toBe(true);
+
+    expect(inter.contains(-1)).toBe(false);
+    expect(inter.contains(101)).toBe(false);
+  });
+
+  xit("should find containing values in negatvie intervals", function() {
+    //TODO: interval broken for negative intervals?
+    inter = new Interval(-1, -99);
+    expect(inter.contains(-1)).toBe(true);
+    expect(inter.contains(-2)).toBe(true);
+    expect(inter.contains(0)).toBe(false);
   });
 });
--- tests/NumberList.test.js
+++ tests/NumberList.test.js
@@ -17,7 +17,13 @@ describe("NumberList", function() {
     var array = [1,2,3];
     var nl = NumberList.fromArray(array);
     expect(nl.length).toBe(3);
+  });
 
+  it("should be accessible", function() {
+    var nl = NumberList.fromArray([1,2,3]);
+    expect(nl[0]).toBe(1);
+    expect(nl[2]).toBe(3);
+    expect(nl[4]).toBe(undefined);
   });
 
   it("should calculate min and max", function() {
@@ -25,6 +31,7 @@ describe("NumberList", function() {
     nl = new NumberList();
     expect(nl.getMin()).toBe(null);
     expect(nl.getMax()).toBe(null);
+
     nl.push(1);
     expect(nl.getMin()).toBe(1);
     expect(nl.getMax()).toBe(1);
@@ -32,7 +39,6 @@ describe("NumberList", function() {
     nl.push(-100);
     expect(nl.getMin()).toBe(-100);
     expect(nl.getMax()).toBe(100);
-
 
     var array = [0,-30,12,30,100,200,4,4,1];
     var nl = NumberList.fromArray(array);
@@ -53,4 +59,46 @@ describe("NumberList", function() {
     nl = NumberList.fromArray([]);
     expect(nl.getAmplitude()).toBe(0);
   });
+
+  it("should calculate sum and product", function() {
+    var nl = NumberList.fromArray([10,20,30,40,50]);
+
+    expect(nl.getSum()).toBe(150);
+    expect(nl.getProduct()).toBe(12000000);
+  });
+
+  it("should sort list", function() {
+    var nl = NumberList.fromArray([20, 10, 40, 50]);
+    var sorted = nl.getSorted();
+
+    expect(sorted[0]).toBe(10);
+    // shouldn't change original lists values
+    expect(nl[0]).toBe(20);
+
+    sorted = nl.getSorted(false);
+    expect(sorted[0]).toBe(50);
+    // shouldn't change original lists values
+    expect(nl[0]).toBe(20);
+  });
+
+  it("should sort indices of list", function() {
+    var nl = NumberList.fromArray([1,3,2]);
+    var indices = nl.getSortIndexes();
+    expect(indices[0]).toBe(1);
+  });
+
+  it("should sort indices of negative list", function() {
+    var nl = NumberList.fromArray([1,-3,2]);
+    var indices = nl.getSortIndexes();
+    expect(indices[0]).toBe(2);
+    expect(indices[2]).toBe(1);
+  });
+
+  it("should sort factor values", function() {
+    var nl = NumberList.fromArray([1,3,2]);
+    var factor = nl.factor(10);
+    expect(factor[0]).toBe(10);
+    expect(factor[1]).toBe(30);
+  });
+
 });
