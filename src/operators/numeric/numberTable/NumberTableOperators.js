322f8ac5d4199cbe59803ae7d6781993076d2616
Jim Vallandingham <vlandham@gmail.com>
2015-08-17 19:52:47 -0700

    Moves NumberTable Conversions to NumberTableConversions

--- src/operators/numeric/numberTable/NumberTableOperators.js
+++ src/operators/numeric/numberTable/NumberTableOperators.js
@@ -3,9 +3,6 @@ import NumberTable from "src/dataStructures/numeric/NumberTable";
 import NumberListOperators from "src/operators/numeric/numberList/NumberListOperators";
 import List from "src/dataStructures/lists/List";
 import Table from "src/dataStructures/lists/Table";
-import Relation from "src/dataStructures/structures/elements/Relation";
-import Node from "src/dataStructures/structures/elements/Node";
-import Network from "src/dataStructures/structures/networks/Network";
 import { instantiateWithSameType } from "src/tools/utils/code/ClassUtils";
 
 /**
@@ -267,80 +264,6 @@ NumberTableOperators.kNN = function(numberTable, propertyList, vectorList, k, ca
   //return results
   return results;
 };
-
-
-
-//TODO: move to NumberTableConversions
-/**
- * @todo finish docs
- */
-NumberTableOperators.numberTableToNetwork = function(numberTable, method, tolerance) {
-  tolerance = tolerance == null ? 0 : tolerance;
-
-  var network = new Network();
-
-  var list0;
-  var list1;
-
-  var i;
-  var j;
-
-  var node0;
-  var node1;
-  var relation;
-
-
-  switch(method) {
-    case 0: // standard deviation
-
-      var sd;
-      var w;
-
-      for(i = 0; numberTable[i + 1] != null; i++) {
-        list0 = numberTable[i];
-
-        if(i === 0) {
-          node0 = new Node(list0.name, list0.name);
-          network.addNode(node0);
-        } else {
-          node0 = network.nodeList[i];
-        }
-
-
-        for(j = i + 1; numberTable[j] != null; j++) {
-          list1 = numberTable[j];
-
-          if(i === 0) {
-            node1 = new Node(list1.name, list1.name);
-            network.addNode(node1);
-          } else {
-            node1 = network.nodeList[j];
-          }
-
-
-
-          list1 = numberTable[j];
-          sd = NumberListOperators.standardDeviationBetweenTwoNumberLists(list0, list1);
-
-          w = 1 / (1 + sd);
-
-          if(w >= tolerance) {
-            relation = new Relation(i + "_" + j, node0.name + "_" + node1.name, node0, node1, w);
-            network.addRelation(relation);
-          }
-        }
-      }
-
-      break;
-    case 1:
-      break;
-    case 2:
-      break;
-  }
-
-  return network;
-};
-
 
 /**
  * calculates the matrix product of two Numbertables
