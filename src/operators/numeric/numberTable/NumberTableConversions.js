be0899523b3a8c701a53c2cc31f57f8eb95cd0bf
Santiago Ortiz <s@moebio.com>
2015-08-31 10:28:09 -0300

    brings numberTableToNewtork

--- src/operators/numeric/numberTable/NumberTableConversions.js
+++ src/operators/numeric/numberTable/NumberTableConversions.js
@@ -1,5 +1,9 @@
 import Polygon from "src/dataStructures/geometry/Polygon";
 import Point from "src/dataStructures/geometry/Point";
+import Network from "src/dataStructures/dataStructures/Network";
+import Node from "src/dataStructures/dataStructures/Node";
+import Relation from "src/dataStructures/dataStructures/Relation";
+import NumberListOperators from "src/operators/numeric/numberList/NumberListOperators";
 
 /**
  * @classdesc NumberTable Conversions
@@ -28,4 +32,75 @@ NumberTableConversions.numberTableToPolygon = function(numberTable) {
   }
 
   return polygon;
+};
+
+//TODO: needs to be improved
+/**
+ * @todo finish docs
+ */
+NumberTableConversions.numberTableToNetwork = function(numberTable, method, tolerance) {
+  tolerance = tolerance == null ? 0 : tolerance;
+
+  var network = new Network();
+
+  var list0;
+  var list1;
+
+  var i;
+  var j;
+
+  var node0;
+  var node1;
+  var relation;
+
+
+  switch(method) {
+    case 0: // standard deviation
+
+      var sd;
+      var w;
+
+      for(i = 0; numberTable[i + 1] != null; i++) {
+        list0 = numberTable[i];
+
+        if(i === 0) {
+          node0 = new Node(list0.name, list0.name);
+          network.addNode(node0);
+        } else {
+          node0 = network.nodeList[i];
+        }
+
+
+        for(j = i + 1; numberTable[j] != null; j++) {
+          list1 = numberTable[j];
+
+          if(i === 0) {
+            node1 = new Node(list1.name, list1.name);
+            network.addNode(node1);
+          } else {
+            node1 = network.nodeList[j];
+          }
+
+
+
+          list1 = numberTable[j];
+          sd = NumberListOperators.standardDeviationBetweenTwoNumberLists(list0, list1);
+
+          w = 1 / (1 + sd);
+
+          if(w >= tolerance) {
+            relation = new Relation(i + "_" + j, node0.name + "_" + node1.name, node0, node1, w);
+            network.addRelation(relation);
+          }
+        }
+      }
+
+      break;
+    case 1:
+      break;
+    case 2:
+      break;
+  }
+
+  return network;
 };
